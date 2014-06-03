42c1d6354374689de2f7329bb86539006471aef9
Charlie Moorhead <charlie.moorhead@gmail.com>
2014-06-03 13:26:42 -0400

    Initial Commit

---
+++ HabitFormer/HabitFormerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HabitFormerAppDelegate.h
+//  HabitFormer
+//
+//  Created by Charlie Moorhead on 6/3/14.
+//  Copyright (c) 2014 Charlie Moorhead. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HabitFormerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HabitFormer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HabitFormer
+//
+//  Created by Charlie Moorhead on 6/3/14.
+//  Copyright (c) 2014 Charlie Moorhead. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HabitFormerAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HabitFormerAppDelegate class]));
+    }
+}
